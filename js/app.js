637a6d56515af57f75a01ffe820d52e0821a4dfd
Davic64 <davidvictoria64@gmail.com>
2021-04-23 02:13:13 -0500

    feat: Adding the size on function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
-const grid = document.querySelector('#grid');
-grid.style.gridTemplateColumns = 'repeat(64, 10px)';
-for(let i = 0; i < 64; i++) {
-    for(let j = 0; j < 64; j++) {
-        const div = document.createElement('div');
-        div.classList.add('square');
-        div.style.width = '10px';
-        div.style.height = '10px';
-        grid.appendChild(div);
-    }
-}
+const grid = document.querySelector("#grid");
 
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'black';
-    })
-})
+const gridUI = (sq) => {
+    grid.style.gridTemplateColumns = `repeat(${sq}, 1fr)`;
+    for (let i = 0; i < sq; i++) {
+        for (let j = 0; j < sq; j++) {
+            const div = document.createElement("div");
+            div.classList.add("square");
+            div.style.width = "100%";
+            div.style.height = "100%";
+            grid.appendChild(div);
+        }
+    }
+
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "black";
+        });
+    });
+};
+
+const setGrid = () => {
+    let sq = Number(prompt("Grid"));
+    gridUI(sq);
+};
+
+gridUI(10);
